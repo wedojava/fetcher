850b2cdbbaa63a33749af44c72583b8d39007157
wedojava <wedojava@gmail.com>
2020-04-25 01:52:26 +0800

    fix windows bug

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/wedojava/fetcher/internal/fetcher"
 	"github.com/wedojava/gears"
@@ -26,22 +29,30 @@ func main() {
 	// fmt.Println("#                                                        #")
 	fmt.Println("#========================================================#")
 	fmt.Printf("\n-> [?] 请输入选项序号: ")
-	var op int
-	fmt.Scanf("%d", &op)
-	if op == 1 {
+	reader := bufio.NewReader(os.Stdin)
+	op, _ := reader.ReadString('\n')
+	// convert CRLF to LF
+	op = strings.Replace(op, "\n", "", -1)
+	op = strings.Replace(op, "\r", "", -1)
+	if strings.Compare("1", op) == 0 {
 		for {
 			fmt.Printf("\n-> [!] 输入网页地址并回车(连续回车退出程序)：\n")
-			var url string
-			fmt.Scanf("%s", &url)
-			if url == "\n" {
-				fmt.Printf("\n\nBye!\n\n")
+			url, _ := reader.ReadString('\n')
+			// convert CRLF to LF
+			url = strings.Replace(url, "\n", "", -1)
+			url = strings.Replace(url, "\r", "", -1)
+			// fmt.Scanf("%s", &url)
+			if strings.Contains(url, "http") {
+				SaveOne(url)
+			} else {
+				fmt.Printf("\nBye!\n\n")
 				return
 			}
-			SaveOne(url)
 		}
-	} else if op == 2 {
+	} else if strings.Compare("2", op) == 0 {
 		fmt.Printf("\n-> [*] 批量内容提取开始......\n")
 	} else {
+		fmt.Printf("\nBye!\n\n")
 		return
 	}
 
