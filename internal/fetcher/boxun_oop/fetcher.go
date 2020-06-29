1f283e215f3e73b6f561e127901cdbf42883e9c6
wedojava <wedojava@gmail.com>
2020-06-29 08:13:08 +0800

    fix bug.

--- internal/fetcher/boxun_oop/fetcher.go
+++ internal/fetcher/boxun_oop/fetcher.go
@@ -68,7 +68,7 @@ func FetchBoxunUrls(url string) []string {
 		return nil
 	} else {
 		for _, v := range reLink.FindAllStringSubmatch(rawBody, -1) {
-			ret_lst = append(ret_lst, "https://www.dwnews.com"+v[1])
+			ret_lst = append(ret_lst, "https://boxun.com"+v[1])
 		}
 		ret_lst = gears.StrSliceDeDupl(ret_lst)
 	}
