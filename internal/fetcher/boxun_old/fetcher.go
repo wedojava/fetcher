854fad8199b8a114318a8a5fa5a2729f8eb3a0e9
wedojava <wedojava@gmail.com>
2020-06-29 20:01:04 +0800

    rename boxun about folders.

---
+++ internal/fetcher/boxun_old/fetcher.go
@@ -0,0 +1,166 @@
+package fetcher
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/wedojava/fetcher/internal/fetcher"
+	"github.com/wedojava/gears"
+)
+
+func FetchBoxun(url string) (*fetcher.ThePost, error) {
+	rawBody, err := gears.HttpGetBody(url, 10)
+	rawBody = gears.ConvertToUtf8(rawBody, "gbk", "utf-8")
+	if err != nil {
+		fmt.Println(err)
+		// log.Fatal(err)
+		return nil, err
+	}
+	domain := "www.boxun.com"
+	site := "www.boxun.com"
+	title := TitleBoxun(rawBody)
+	// get contents
+	body, err := FmtBodyBoxun(rawBody)
+	if err != nil {
+		fmt.Println(err)
+		// log.Fatal(err)
+		return nil, err
+	}
+
+	date := DateBoxun(url)
+
+	post := fetcher.ThePost{Site: site, Domain: domain, URL: url, Title: title, Body: body, Date: date}
+
+	return &post, nil
+}
+
+func DateBoxun(url string) string {
+	rawdate := filepath.Base(url)
+	var Y, M, D, hh, mm int
+	var err error
+	if Y, err = strconv.Atoi(rawdate[:4]); err != nil {
+		Y = 2000
+		fmt.Println(rawdate[:4], "is not a Year., set it to 2000")
+	}
+	if M, err = strconv.Atoi(rawdate[4:6]); err != nil {
+		M = 0
+		fmt.Println(rawdate[4:6], "is not a Month., set it to 0")
+	}
+	if D, err = strconv.Atoi(rawdate[6:8]); err != nil {
+		D = 0
+		fmt.Println(rawdate[6:8], "is not a Date., set it to 0")
+	}
+	if hh, err = strconv.Atoi(rawdate[8:10]); err != nil {
+		hh = 0
+		fmt.Println(rawdate[8:10], "is not a Hour., set it to 0")
+	}
+	if mm, err = strconv.Atoi(rawdate[10:12]); err != nil {
+		mm = 0
+		fmt.Println(rawdate[10:12], "is not a Minute., set it to 0")
+	}
+	if err != nil {
+		fmt.Println("err date fetch from url: ", url)
+	}
+	if Y < 1999 || Y > 2499 {
+		Y = 2000
+		// fmt.Println(Y, "is not a integer of Year, set it to 2000")
+	}
+	if M < 0 || M >= 12 {
+		M = 12
+		// fmt.Println(M, "is not a integer of Month, set it to 12")
+	}
+	if D < 0 || D >= 31 {
+		D = 31
+		// fmt.Println(D, "is not a integer of Date, set it to 31")
+	}
+	if hh < 0 || hh > 23 {
+		hh = 23
+		// fmt.Println(hh, "is not a integer of Hour, set it to 23")
+	}
+	if mm < 0 || mm > 59 {
+		mm = 59
+		// fmt.Println("err date fetch from url: ", url)
+		// fmt.Println(mm, "is not a integer of Minute, set it to 59")
+	}
+	return fmt.Sprintf("%02d-%02d-%02dT%02d:%02d:%02dZ", Y, M, D, hh, mm, 0)
+
+}
+
+func TitleBoxun(raw string) string {
+	var a = regexp.MustCompile(`(?m)<title>(.*?)</title>`)
+	rt := a.FindStringSubmatch(raw)
+	if rt != nil {
+		s := strings.TrimSpace(rt[1])
+		fetcher.ReplaceIllegalChar(&s)
+		return s
+	} else {
+		return ""
+	}
+}
+
+func FetchBoxunUrls(url string) []string {
+	rawBody, err := gears.HttpGetBody(url, 10)
+	if err != nil {
+		fmt.Println(err)
+		// log.Fatal(err)
+	}
+	var ret_lst []string
+	var reLink = regexp.MustCompile(`(?m)<li><a href="(/news/\w*/.*?)" target=_blank>`)
+	lst := reLink.FindAllStringSubmatch(rawBody, -1)
+	if lst == nil {
+		fmt.Printf("\n[-] fetcher.FetchBoxunUrls(%s) regex matched nothing.\n", url)
+		return nil
+	} else {
+		for _, v := range reLink.FindAllStringSubmatch(rawBody, -1) {
+			ret_lst = append(ret_lst, "https://boxun.com"+v[1])
+		}
+		ret_lst = gears.StrSliceDeDupl(ret_lst)
+	}
+
+	return ret_lst
+}
+
+// FmtBodyBoxun focus on dwnews, it can extract raw body string via regexp and then, format the news body to markdowned string.
+func FmtBodyBoxun(rawBody string) (string, error) {
+	if rawBody == "" {
+		return "", errors.New("\n[-] FmtBodyBoxun() parameter is nil!\n")
+	}
+	var ps []string
+	var body string
+	var re = regexp.MustCompile(`(?m)<!--bodystart-->([^\^]*)<!--bodyend-->`)
+	for _, v := range re.FindAllStringSubmatch(rawBody, -1) {
+		ps = append(ps, v[1])
+	}
+	if len(ps) == 0 {
+		if regexp.MustCompile(`(?m)<video.*?>`).FindAllString(rawBody, -1) != nil {
+			return "", errors.New("\n[-] fetcher.FmtBodyBoxun() Error: this is a video page.\n")
+		}
+		return "", errors.New("\n[-] fetcher.FmtBodyBoxun() Error: regex matched nothing.\n")
+	} else {
+		for _, p := range ps {
+			body += p + "  \n"
+		}
+	}
+	a := regexp.MustCompile(`<BR>`)
+	bodySlice := a.Split(body, -1)
+	body = ""
+	for _, v := range bodySlice {
+		re = regexp.MustCompile(`<table([^\^]*)</table>`)
+		v = re.ReplaceAllString(v, "")
+		re = regexp.MustCompile(`&nbsp;`)
+		v = re.ReplaceAllString(v, "")
+		re = regexp.MustCompile(`<div(.*?)</div>`)
+		v = re.ReplaceAllString(v, "")
+		re = regexp.MustCompile(`<img(.*?)>`)
+		v = re.ReplaceAllString(v, "")
+		if strings.TrimSpace(v) == "" {
+			continue
+		}
+		body += v + "  \n"
+	}
+	return body, nil
+}
---
+++ internal/fetcher/boxun_old/fetcher_test.go
@@ -0,0 +1,68 @@
+package fetcher
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/wedojava/gears"
+)
+
+func checkFetch(t *testing.T, _got, _want string) {
+	t.Helper()
+	if _got != _want {
+		t.Errorf("\ngot %v\nwant %v\n", _got, _want)
+	}
+}
+
+func TestFetch(t *testing.T) {
+	t.Run("test get title and body: ", func(t *testing.T) {
+		got, _ := FetchBoxun("https://boxun.com/news/gb/intl/2020/06/202006081127.shtml")
+		wantTitle := "无公正 不停歇：欧洲各大城市加入反警暴浪潮"
+		wantDomain := "www.boxun.com"
+		wantSite := "www.boxun.com"
+		wantDate := "2020-06-08T11:27:00Z"
+		checkFetch(t, got.Title, wantTitle)
+		checkFetch(t, got.Domain, wantDomain)
+		checkFetch(t, got.Site, wantSite)
+		checkFetch(t, got.Date, wantDate)
+	})
+}
+
+func TestFmtBodyBoxun(t *testing.T) {
+	rawBody, err := gears.HttpGetBody("https://boxun.com/news/gb/intl/2020/06/202006160353.shtml", 10)
+	// rawBody, err := gears.HttpGetBody("https://boxun.com/news/gb/pubvp/2020/06/202006110029.shtml", 10)
+	// rawBody, err := gears.HttpGetBody("https://boxun.com/news/gb/intl/2020/06/202006081127.shtml", 10)
+	rawBody = gears.ConvertToUtf8(rawBody, "gbk", "utf-8")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := FmtBodyBoxun(rawBody)
+	// if strings.Contains(got, "<!--bodystart-->") {
+	if strings.Contains(got, "三星在西安工厂展开巨额投资，也与中美对立不无关系。成为竞争对手的中国的半导体厂商无法引进美国造设备，难以顺利实现尖端产品的稳定量产。结果，各智能手机企业不得不依赖三星的西安工厂作为供应企业。") {
+		// if strings.Contains(got, "還有「犯我強漢者，雖遠必誅」，「壯志飢餐胡虜肉」等，都是漢人愛國將領的名言。西方國家要記住這些話。台灣人被選上總統，對外省權貴來說，也是「犯漢」，一定要用各種手段進行報復。") {
+		// if strings.Contains(got, "意大利罗马的人民广场也以8分钟的寂静来悼念弗洛伊德，选择8分钟，是因为弗洛伊德被白人警察用膝盖压住脖子时间超过8分钟。在集体默哀之后，人们继续呼喊反种族歧视的口号。受到非法难民浪潮等社会政治经济问题的冲击，意大利向民粹偏移，本次罗马游行的人群当中不少都是移民或者非法入境难民的身份，他们表示，在意大利正常生活，真的很难。") {
+	} else {
+		t.Errorf("got: %v", got)
+	}
+	if strings.Contains(got, `<!--bodystart-->`) {
+		t.Errorf("body filter not better, still contain: <!--bodystart-->...")
+	}
+	if strings.Contains(got, `</table>`) {
+		t.Errorf("body filter not better, still contain: <table ...>...</table>")
+	}
+	fmt.Print("Test pass.")
+}
+
+func TestFetchUrls(t *testing.T) {
+	t.Run("get urls count.", func(t *testing.T) {
+		got := FetchBoxunUrls("https://www.boxun.com/rolling.shtml")
+		want := 179
+		if len(got) == want {
+			fmt.Print("Test pass.")
+		} else {
+			t.Errorf("\nGot %v\n Want %v", len(got), want)
+		}
+
+	})
+}
