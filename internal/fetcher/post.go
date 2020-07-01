d859b10a54f3bca82a9deb360442586f739cc7f8
wedojava <wedojava@gmail.com>
2020-07-01 21:17:03 +0800

    needed to test if save right.

--- internal/fetcher/post.go
+++ internal/fetcher/post.go
@@ -1,21 +1,26 @@
 package fetcher
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
+	"path/filepath"
 	"time"
 
+	"github.com/wedojava/gears"
 	"golang.org/x/net/html"
 )
 
 type Post struct {
-	Domain string
-	URL    *url.URL
-	DOC    *html.Node
-	Raw    []byte
-	Title  string
-	Body   string
-	Date   string
+	Domain   string
+	URL      *url.URL
+	DOC      *html.Node
+	Raw      []byte
+	Title    string
+	Body     string
+	Date     string
+	Filename string
 }
 
 type Paragraph struct {
@@ -50,9 +55,35 @@ func (p *Post) SetPost() error {
 	if err := p.SetTitle(); err != nil {
 		return err
 	}
+	// set Filename
+	if err := p.SetFilename(); err != nil {
+		return err
+	}
 	// set Body (get and format body)
 	if err := p.SetBody(); err != nil {
 		return err
 	}
+	return nil
+}
+
+func (p *Post) SavePost() error {
+	folderPath := filepath.Join("wwwroot", p.Domain)
+	gears.MakeDirAll(folderPath)
+	filepath := filepath.Join(folderPath, p.Filename)
+	if !gears.Exists(filepath) {
+		err := ioutil.WriteFile(filepath, []byte(p.Body), 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *Post) SetFilename() error {
+	t, err := time.Parse(time.RFC3339, p.Date)
+	if err != nil {
+		return err
+	}
+	p.Filename = fmt.Sprintf("[%02d.%02d][%02d%02dH]%s.txt", t.Month(), t.Day(), t.Hour(), t.Minute(), p.Title)
 	return nil
 }
