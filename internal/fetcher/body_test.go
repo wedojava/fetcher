ab007df16575898b5a2a2ec29c2ead6479f79e6d
wedojava <wedojava@gmail.com>
2020-07-09 20:34:19 +0800

    don't save picture info text to the body

--- internal/fetcher/body_test.go
+++ internal/fetcher/body_test.go
@@ -10,7 +10,8 @@ func TestSetBody(t *testing.T) {
 	// prepare
 	// p := PostFactory("https://www.dwnews.com/%E5%85%A8%E7%90%83/60202451") // The right one
 	// p := PostFactory("https://www.dwnews.com/%E5%8F%B0%E6%B9%BE/60202352") // The wrong one
-	p := PostFactory("https://www.rfa.org/mandarin/yataibaodao/shehui/hj-07022020095655.html")
+	// p := PostFactory("https://www.rfa.org/mandarin/yataibaodao/shehui/hj-07022020095655.html")
+	p := PostFactory("https://www.dwnews.com/经济/60203253") // The wrong one
 	raw, doc, err := GetRawAndDoc(p.URL, 1*time.Minute)
 
 	if err != nil {
@@ -26,8 +27,10 @@ func TestSetBody(t *testing.T) {
 }
 
 func TestDwnews(t *testing.T) {
-	p := PostFactory("https://www.dwnews.com/%E5%85%A8%E7%90%83/60202451") // The right one
+	// p := PostFactory("https://www.dwnews.com/%E5%85%A8%E7%90%83/60202451") // The right one
 	// p := PostFactory("https://www.dwnews.com/%E5%8F%B0%E6%B9%BE/60202352") // The wrong one
+	// p := PostFactory("https://www.dwnews.com/全球/60203234")
+	p := PostFactory("https://www.dwnews.com/经济/60203253")
 	raw, doc, err := GetRawAndDoc(p.URL, 1*time.Minute)
 	if err != nil {
 		t.Errorf("GetRawAndDoC error: %v", err)
